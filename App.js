592bd421ffb8c111df7c2b77a6773c45116551a9
lasith herath <lasithherath00@gmail.com>
2024-02-29 09:22:40 +0530

    fix input fields

--- App.js
+++ App.js
@@ -1,53 +1,53 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from '../Fyp/components/LoginScreen';
 import FormScreen from './components/FormScreen';
 import RequestScreen from '../Fyp/components/RequestScreen';
 import SignUpScreen from '../Fyp/components/SignUpScreen';
 import CameraScreen from './components/CameraScreen ';
 import BottomNavigation from './components/BottomNavigation';
-
+import BottomTabs from './components/BottomTabs';
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-<Stack.Navigator>
+      <Stack.Navigator>
         <Stack.Screen
           name="LoginScreen"
           component={LoginScreen}
-          options={{title: 'Welcome'}}
+          options={{ title: 'Welcome' }}
         />
         <Stack.Screen
           name="FormScreen"
-          component={FormScreen}
-          options={{title: 'Welcome'}}
+          component={BottomTabs}
+          options={{ title: 'Welcome' }}
         />
         <Stack.Screen
           name="RequestScreen"
           component={RequestScreen}
-          options={{title: 'Welcome'}}
+          options={{ title: 'Welcome' }}
         />
         <Stack.Screen
           name="SignUpScreen"
           component={SignUpScreen}
-          options={{title: 'Welcome'}}
+          options={{ title: 'Welcome' }}
         />
         {/* <Stack.Screen
           name="BottomNavigation"
           component={BottomNavigation}
           options={{title: 'Welcome'}}
         /> */}
-        
+
         <Stack.Screen
-  name="CameraScreen" 
-  component={CameraScreen} 
-  options={{title: 'Welcome'}}
-/>
+          name="CameraScreen"
+          component={CameraScreen}
+          options={{ title: 'Welcome' }}
+        />
       </Stack.Navigator>
-  </NavigationContainer>
+    </NavigationContainer>
   );
 }
 
